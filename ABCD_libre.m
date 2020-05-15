e6f6297f0fe6585d5156480a9848c63370a76fb1
PabloHsi <49988407+PabloHsi@users.noreply.github.com>
2020-05-15 04:31:18 -0300

    Add files via upload

---
+++ ABCD_libre.m
@@ -0,0 +1,13 @@
+function [matriz] = ABCD_libre(z)
+%function [A,B,C,D] = ABCD_libre(z)
+%Devuelvo los parametros ABCD dada la posicion z en camino libre
+
+matriz = [1 z ; 0 1];
+
+%A = 1;
+%B = z;
+%C = 0;
+%D = 1;
+
+end
+
