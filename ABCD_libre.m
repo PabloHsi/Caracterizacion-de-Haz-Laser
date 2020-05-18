13448a6f8efb33404b5736915bc7a53612e28278
PabloHsi <49988407+PabloHsi@users.noreply.github.com>
2020-05-18 01:09:07 -0300

    Add files via upload

--- ABCD_libre.m
+++ ABCD_libre.m
@@ -1,13 +1,7 @@
 function [matriz] = ABCD_libre(z)
-%function [A,B,C,D] = ABCD_libre(z)
 %Devuelvo los parametros ABCD dada la posicion z en camino libre
 
 matriz = [1 z ; 0 1];
-
-%A = 1;
-%B = z;
-%C = 0;
-%D = 1;
 
 end
 
--- arreglo.m
+++ arreglo.m
@@ -1,5 +1,4 @@
 function [matriz] = arreglo(z,d0,f1,f2)
-%function [A,B,C,D] = arreglo(z,d0,f1,f2)
 %Hasta d0 es campo libre
 %En d0 hay una lente f1
 %En d0+d hay una lente f2
@@ -13,10 +12,5 @@ function [matriz] = arreglo(z,d0,f1,f2)
     else 
         matriz = [1 (z-d-d0) ; 0 1]*[1 0; -1/f2 1]*[1 d;0 1]*[1 0;(-1/f1) 1]*[1 d0;0 1];
     end
-    
-    %A=matriz(1,1);
-    %B=matriz(1,2);
-    %C=matriz(2,1);
-    %D=matriz(2,2);
     
 end
